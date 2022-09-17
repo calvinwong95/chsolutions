283876c521556dc67a2c95b77789007cf458a207
calvinwong95 <calvinwongch95@gmail.com>
2022-09-17 20:48:31 +0800

    changed to hashrouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import MainPage from "./pages/MainPage";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import { PageStateProvider } from "./context/PageStateContext";
 
 function App() {
