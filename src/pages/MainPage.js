99decf4a11cb0ab08ab5069541b6ff8dd1416070
calvinwong95 <calvinwongch95@gmail.com>
2022-09-20 13:09:35 +0800

    fix: change style

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -5,45 +5,90 @@ import AboutMe from "./AboutMe";
 import Contact from "./Contact";
 import DefaultPage from "./DefaultPage";
 import Solution from "./Solution";
-
+import { Box } from "@mui/material";
 import Spline from "@splinetool/react-spline";
 import { motion } from "framer-motion";
 
+import { useTheme } from "@mui/material/styles";
+import useMediaQuery from "@mui/material/useMediaQuery";
+
 const MainPage = () => {
+  const theme = useTheme();
+  const matches = useMediaQuery(theme.breakpoints.up("sm"));
+
   const { pageState, setPageState } = useContext(PageStateContext);
 
   return (
-    <div
-      style={{ width: "100vw", height: "100vh", backgroundColor: "#111116" }}
+    <Box
+      style={{
+        width: "100vw",
+        backgroundColor: "#111116",
+      }}
     >
       <Navbar setPageState={setPageState} />
-      <div
+      <Box
         className="content"
         style={{
           display: "flex",
-          flexDirection: "row",
-          width: "100vw",
-          height: "90vh",
+          flexDirection: matches ? "row" : "column",
         }}
       >
-        <div className="containerLeft">
-          <motion.div
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            transition={{ duration: 0.5, delay: 2 }}
-            style={{ width: "100%", height: "100%" }}
-          >
-            <Spline scene="https://prod.spline.design/FPQV2VLoRYVD1v3V/scene.splinecode" />
-          </motion.div>
-        </div>
-        <div className="containerRight">
+        <Box
+          sx={{
+            width: matches ? "40%" : "100%",
+            height: "90vh",
+          }}
+        >
+          {pageState === "main-page" || pageState === "about-me" ? (
+            <Box
+              component={motion.div}
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ duration: 0.5, delay: 2 }}
+              sx={{ height: 1 }}
+            >
+              <Spline scene="https://prod.spline.design/FPQV2VLoRYVD1v3V/scene.splinecode" />
+            </Box>
+          ) : null}
+          {pageState === "work" ? (
+            <Box
+              component={motion.div}
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ duration: 0.5, delay: 2 }}
+              sx={{ height: 1 }}
+            >
+              <Spline scene="https://prod.spline.design/DPYr3EM64eIGHjeS/scene.splinecode" />
+            </Box>
+          ) : null}
+
+          {pageState === "contact" && (
+            <Box
+              component={motion.div}
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ duration: 0.5, delay: 2 }}
+              sx={{ height: 1 }}
+            >
+              <Spline scene="https://prod.spline.design/7FYE-Xma1DX5gMJX/scene.splinecode" />
+            </Box>
+          )}
+        </Box>
+        <Box
+          sx={{
+            backgroundColor: "#111116",
+            width: matches ? "60%" : "100%",
+            height: "90vh",
+            overflowY: pageState === "work" ? "auto" : null,
+          }}
+        >
           {pageState === "main-page" && <DefaultPage />}
           {pageState === "about-me" && <AboutMe />}
           {pageState === "work" && <Solution />}
           {pageState === "contact" && <Contact />}
-        </div>
-      </div>
-    </div>
+        </Box>
+      </Box>
+    </Box>
   );
 };
 
--- src/pages/Solution.js
+++ src/pages/Solution.js
@@ -1,63 +1,62 @@
-import React from "react";
-import PlanningSolution from "../components/PlanningSolution";
+import React, { useState } from "react";
 import { motion } from "framer-motion";
-import Spline from "@splinetool/react-spline";
+import LandingPage from "../components/LandingPage";
+import Animation from "../components/Animation";
+import Maintenance from "../components/Maintenance";
+import { Box } from "@mui/material";
 
 const Solution = () => {
+  const [showWork, setShowWork] = useState(1);
+  document.addEventListener("keydown", function (event) {
+    if (event.code === "Digit1") {
+      setShowWork(1);
+    }
+    if (event.code === "Digit2") {
+      setShowWork(2);
+    }
+    if (event.code === "Digit3") {
+      setShowWork(3);
+    }
+  });
   return (
     <div className="work-container">
-      <motion.div
+      <Box
+        component={motion.div}
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
         transition={{ duration: 0.3, delay: 0.3 }}
       >
-        <div className="Landing Page & Website">
-          <h1 style={{ color: "white", textDecoration: "underline" }}>
-            {"LANDING PAGE & WEBSITE"}
-          </h1>
-          <h3 style={{ color: "white", marginTop: 20 }}>
-            Our workflow starts with planning and structuring the layout and
-            deliverables.
-            <br /> Once completed, we will then move to the designing stage of
-            the prototype.
-            <br /> Lastly, we will develop and deliver the finalized product.
-          </h3>
-          <div style={{ marginTop: 50, marginBottom: 50 }}>
-            <PlanningSolution />
-          </div>
-        </div>
-        <div className="3D Animation">
-          <h1 style={{ color: "white", textDecoration: "underline" }}>
-            {"3D Animation"}
-          </h1>
-          <h3 style={{ color: "white", marginTop: 20, marginBottom: 50 }}>
-            We create and design 3D elements to make your website attractive and
-            improve user's experience.
-            <br />
-            Feel free to press the button with your keyboard!
-          </h3>
-          <motion.div
+        {showWork === 1 && (
+          <Box
+            component={motion.div}
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
             transition={{ duration: 0.3, delay: 0.3 }}
-            style={{
-              display: "flex",
-              justifyContent: "center",
-            }}
           >
-            <Spline scene="https://prod.spline.design/DPYr3EM64eIGHjeS/scene.splinecode" />
-          </motion.div>
-        </div>
-        <div className="Maintenance">
-          <h1 style={{ color: "white", textDecoration: "underline" }}>
-            {"Maintenance"}
-          </h1>
-          <h3 style={{ color: "white", marginTop: 20 }}>
-            Trouble in navigating your website and needing to consistently
-            update the content? <br /> No worries! <br /> We can do it for you!
-          </h3>
-        </div>
-      </motion.div>
+            <LandingPage />
+          </Box>
+        )}
+        {showWork === 2 && (
+          <Box
+            component={motion.div}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 0.3, delay: 0.3 }}
+          >
+            <Animation />
+          </Box>
+        )}
+        {showWork === 3 && (
+          <Box
+            component={motion.div}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 0.3, delay: 0.3 }}
+          >
+            <Maintenance />
+          </Box>
+        )}
+      </Box>
     </div>
   );
 };
